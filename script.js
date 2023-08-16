46297245afb686bb45ae9a092d8a7639868da45c
Matheus <mathieus_n.7@hotmail.com>
2023-08-16 17:02:14 -0300

    Fix 'addBookToLibrary()' function
    Book object now has a prototype.

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 const myLibrary = [];
 
+Book.prototype = {
+    info : function() {
+        return `${this.title} by ${this.author}, ${this.pages}, ${this.read}`
+    }
+}
+
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
-    this.info = function() {
-        return `${title} by ${author}, ${pages}, ${read}`
-    }
 }
 
 function addBookToLibrary() {
-    //do stuff gere
+    let title = prompt('Please enter a title');
+    let author = prompt('Please tell me the author');
+    let pages = prompt('How many pages');
+    let read = prompt('Already read or not');
+
+    const book01 = new Book(title, author, pages, read);
+
+    return myLibrary.push(book01);
 }
